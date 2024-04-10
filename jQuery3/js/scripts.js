0462bafd34aa2b6b6bede2b3c13a2afdcbd040ee
RealmOfBlue <adreagan@my.chattanoogastate.edu>
2024-04-10 14:24:20 -0400

    billy mac's menu page next step: footer

--- jQuery3/js/scripts.js
+++ jQuery3/js/scripts.js
@@ -27,3 +27,60 @@ $( function() {
       event.preventDefault();
     } );
   } );
+
+
+  $( function() {
+  
+    $(".billy-mac").click(function () {
+      $(".selected-img").attr("src", "images/entrees/billy-mac.png");
+      $(".selected-desc").attr("src", "images/desc/billy-mac-desc.png");
+    });
+    $(".billy-basic").click(function () {
+        $(".selected-img").attr("src", "images/entrees/billy-basic.png");
+        $(".selected-desc").attr("src", "images/desc/billy-basic-desc.png");
+      });
+    $(".double-troubles").click(function () {
+        $(".selected-img").attr("src", "images/entrees/double-troubles.png");
+        $(".selected-desc").attr("src", "images/desc/double-troubles-desc.png");
+    });
+    $(".chicken-dinner").click(function () {
+          $(".selected-img").attr("src", "images/entrees/chicken-dinner.png");
+          $(".selected-desc").attr("src", "images/desc/chicken-dinner-desc.png");
+    });
+    $(".hot-diggity-dogs").click(function () {
+        $(".selected-img").attr("src", "images/entrees/hot-diggity-dogs.png");
+        $(".selected-desc").attr("src", "images/desc/hot-diggity-dogs-desc.png");
+    });
+    $(".chili-cheesers").click(function () {
+          $(".selected-img").attr("src", "images/entrees/chili-cheesers.png");
+          $(".selected-desc").attr("src", "images/desc/chili-cheesers-desc.png");
+    });
+} );
+
+$( function() {
+  
+    $(".billy-mac").click(function () {
+      $(".selected-img").attr("src", "images/entrees/billy-mac.png");
+      $(".selected-desc").attr("src", "images/desc/billy-mac-desc.png");
+    });
+    $(".billy-basic").click(function () {
+        $(".selected-img").attr("src", "images/entrees/billy-basic.png");
+        $(".selected-desc").attr("src", "images/desc/billy-basic-desc.png");
+      });
+    $(".double-troubles").click(function () {
+        $(".selected-img").attr("src", "images/entrees/double-troubles.png");
+        $(".selected-desc").attr("src", "images/desc/double-troubles-desc.png");
+    });
+    $(".chicken-dinner").click(function () {
+          $(".selected-img").attr("src", "images/entrees/chicken-dinner.png");
+          $(".selected-desc").attr("src", "images/desc/chicken-dinner-desc.png");
+    });
+    $(".hot-diggity-dogs").click(function () {
+        $(".selected-img").attr("src", "images/entrees/hot-diggity-dogs.png");
+        $(".selected-desc").attr("src", "images/desc/hot-diggity-dogs-desc.png");
+    });
+    $(".chili-cheesers").click(function () {
+          $(".selected-img").attr("src", "images/entrees/chili-cheesers.png");
+          $(".selected-desc").attr("src", "images/desc/chili-cheesers-desc.png");
+    });
+} );
