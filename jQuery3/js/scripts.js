684941ac313b42721ed9a993c438d987e945e1c5
RealmOfBlue <adreagan@my.chattanoogastate.edu>
2024-04-09 20:56:05 -0400

    billy mac's - almost done with the menu

--- jQuery3/js/scripts.js
+++ jQuery3/js/scripts.js
@@ -1,11 +1,29 @@
 $( function() {
-    $( "#accordion" ).accordion({
+    $( ".desktop" ).accordion({
       collapsible: true,
       active: false,
       heightStyle: SVGTextContentElement,
-      widthStyle: SVGTextContentElement,
         event: "mouseover"
         }).on('mouseleave', function () {
             $(this).accordion("option", "active", false);
         });
+
+        $( ".item" ).accordion({
+            collapsible: true,
+            active: false,
+              });
+
+        $( ".item-full" ).accordion({
+                collapsible: true,
+                active: false,
+
+        });
+                
+  } );
+
+  $( function() {
+    $( ".widget input[type=submit], .widget a, .widget button" ).button();
+    $( "button, input, a" ).on( "click", function( event ) {
+      event.preventDefault();
+    } );
   } );
